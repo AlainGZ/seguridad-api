e644e22a456a23bf7d445e393ddc7942e529ab67
AlainGZ <devalgz955@gmail.com>
2025-03-31 12:33:04 -0500

    Se crea el proyecto

---
+++ src/main/java/com/example/seguridad_api/SeguridadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.seguridad_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seguridad_api/SeguridadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seguridad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
